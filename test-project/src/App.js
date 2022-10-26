0db9b823b4256cb5797a993a2a184db4c2069713
Kanghj <rkdguswns0803@gmail.com>
2022-10-26 11:38:30 +0900

    Grid

--- test-project/src/App.js
+++ test-project/src/App.js
@@ -5,6 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComp from "./components/NavbarComp";
 import NavComp from "./components/NavComp";
 import CardComp from "./components/CardComp";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
@@ -21,6 +24,15 @@ function App() {
       >
         Button as link
       </Button>
+      <Container>
+        <Row>
+          <Col xs="6">1 of 3</Col>
+          <Col md="auto">Variable width content</Col>
+          <Col xs lg="2">
+            3 of 3
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
